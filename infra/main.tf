b202a6d2530047514a7d14ef66b7537d25c7783f
kanekotic <me@kanekotic.com>
2023-02-28 09:34:21 +0100

    ALVARO | try fix endpoint

--- infra/main.tf
+++ infra/main.tf
@@ -64,7 +64,7 @@ resource "aws_api_gateway_integration" "lambda" {
   resource_id = "${aws_api_gateway_method.proxy.resource_id}"
   http_method = "${aws_api_gateway_method.proxy.http_method}"
 
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.lambda.invoke_arn}"
 }
