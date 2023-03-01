c826214e283d6a90ad7aee419f982f8686699f8f
kanekotic <me@kanekotic.com>
2023-03-01 15:12:34 +0100

    ALVARO | added user route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const health = require("./rutas/health")
+const users = require("./rutas/users")
 
 module.exports = () => {
     const app = express()
+    app.use(express.json());
     app.use("/health", health)
+    app.use("/users", users)
     return app
 }
